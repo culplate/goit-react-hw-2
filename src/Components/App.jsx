00390c81f248bf132dcc5eaf3b7d8cd41ac719b8
culplate <culplate@gmail.com>
2024-01-24 23:19:29 +0100

    setup

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react';
 import './App.css';
-
+import { SpeedInsights } from '@vercel/speed-insights/next';
 function App() {
-  return <></>;
+  return (
+    <>
+      <SpeedInsights />
+    </>
+  );
 }
 
 export default App;
